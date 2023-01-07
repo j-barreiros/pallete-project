4760ae69c46161dc5e14c43cc31096ba2640d67d
j-novaes <joaovictor.nb21@hotmail.com>
2023-01-07 14:12:17 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// Hooks
+import { useState } from 'react'
+
+// Style
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
