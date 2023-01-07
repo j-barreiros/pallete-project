135de2dbdb4dc3d67190432f9bbb456b24b520b5
j-novaes <joaovictor.nb21@hotmail.com>
2023-01-07 23:50:00 +0000

    Edited App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import { useState } from 'react'
 // Style
 import './App.css'
 
+// Components
+import Navbar from './components/Navbar/Navbar'
+import PalleteDisplay from './components/PalleteDisplay/PalleteDisplay'
+
+// Context
+import { PalleteContextProvider } from './context/PalletesContext'
+
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <PalleteContextProvider>
+        <Navbar />
+        <section>
+          <PalleteDisplay />
+        </section>
+      </PalleteContextProvider>
+    </main>
   )
 }
 
