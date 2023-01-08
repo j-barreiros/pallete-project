23df88dff214c19b659323cbf3a59f514c22a569
j-novaes <joaovictor.nb21@hotmail.com>
2023-01-08 23:52:18 +0000

    Created GlobalStyled and defined project font

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap');
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
+`
+
+export default GlobalStyle;
