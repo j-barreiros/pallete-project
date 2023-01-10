356a4d3dbc11da4c9809e9ccace30152f153ee15
j-novaes <joaovictor.nb21@hotmail.com>
2023-01-10 23:19:47 +0000

    Created CollectionBar Component

---
+++ src/components/CollectionBar/CollectionBar.tsx
@@ -0,0 +1,14 @@
+import StyledCollectionBar from "./StyledCollectionBar";
+
+const CollectionBar = () => {
+    return (
+        <StyledCollectionBar>
+            <h2 className='title'>Collection</h2>
+            <section className='collection-display'>
+                
+            </section>
+        </StyledCollectionBar>
+    )
+}
+
+export default CollectionBar;
---
+++ src/components/CollectionBar/StyledCollectionBar.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledCollectionBar = styled.section`
+    width: 270px;
+    height: 100vh;
+    margin-top: 50px;
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    right: 0px;
+    background-color: gray;
+`
+
+export default StyledCollectionBar;
